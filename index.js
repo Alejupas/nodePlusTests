f7edd78df9fc73f2184ff96e3e2a0e05f2a0fd9d
Mindaugas Kapickas <mindaugas.kapickas@visma.com>
2021-08-05 11:10:25 +0300

    wordSearch 5 done

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const rangeA = require('./problemsTasks/rangeA1')
-console.log('rangeA',rangeA(4,9))
+console.log('1. rangeA',rangeA(4,9))
 
 const rangeB = require('./problemsTasks/rangeB1')
-console.log('rangeB',rangeB(12,19))
+console.log('1. rangeB',rangeB(12,19))
 
 const sumA = require('./problemsTasks/sumA2')
-console.log('sumA',sumA([1,2,3,4,200]))
+console.log('2. sumA',sumA([1,2,3,4,200]))
 
 const sumB = require('./problemsTasks/sumB2')
-console.log('sumB', sumB([75, 150, 360, 50, 100]))
+console.log('2. sumB', sumB([75, 150, 360, 50, 100]))
 
 const getMottoA = require('./problemsTasks/houseA3')
-console.log('getMottoA', getMottoA('Stark'))
+console.log('3. getMottoA', getMottoA('Stark'))
 
 const getMottoB = require('./problemsTasks/houseB3');
-console.log('getMottoB', getMottoB('Greyjoy'))
+console.log('3. getMottoB', getMottoB('Greyjoy'))
 
 const noDuplicates = require('./problemsTasks/noDuplicates4')
-console.log('No Duplicates', noDuplicates([2,2,4,1,5,6,6,4,3,8,8,8]))
+console.log('4. No Duplicates', noDuplicates([2,2,4,1,5,6,6,4,3,8,8,8]))
+
+const wordSearch = require('./problemsTasks/wordSearch5')
+console.log('5. WordSearch', wordSearch('This wAS Garfield', 'Was'))
---
+++ problemsTasks/wordSearch5.js
@@ -0,0 +1,11 @@
+/**
+ * Create a function wordSearch(w) that searches to see whether a word w is present in
+the given text variable. Please note it has to be a full word and case insensitive.
+ */
+
+module.exports = (text, word) => {
+    if(typeof text !== 'string' || typeof word !== 'string') return console.log('It has to be a string.')
+
+    const regex = new RegExp(`\\b${word}\\b`, "gi")
+    return regex.test(text)
+}
