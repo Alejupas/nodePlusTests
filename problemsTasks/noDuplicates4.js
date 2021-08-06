03ce6d543dc84edf3604a589a7bbdb3f6281e002
Mindaugas Kapickas <mindaugas.kapickas@visma.com>
2021-08-06 11:06:41 +0300

    noDuplicates4 test done

--- problemsTasks/noDuplicates4.js
+++ problemsTasks/noDuplicates4.js
@@ -1,13 +1,14 @@
 /**Create a function which takes an array and returns an array with all duplicates
 removed. */
 const noDuplicates = (arr) => {
-    if(typeof arr !== 'object') return console.log('It should only be an array.')
+   if (!Array.isArray(arr)) return 'It should only be an array.';
 
     const newArr = Array.from(new Set(arr))
     return newArr;
 
     // alternative way (as well can be filter method/prototype using indexof or can be reduce method/prototype)
 
+    // pure function when original arr is not modified:
     // const uniqueArr = new Set(arr);
     // const newUniqueArr = [...uniqueArr]
     // return newUniqueArr
--- problemsTasks/noDuplicates4.test.js
+++ problemsTasks/noDuplicates4.test.js
@@ -4,4 +4,12 @@ describe('4. noDuplicates function', ()=>{
     test('should return no duplicates array', ()=> {
         expect(noDuplicates([2,3,4,2,3,4,1])).toEqual([2,3,4,1])
     })
+
+    test('should return error', ()=> {
+        expect(noDuplicates(2,4,6,4,2)).toEqual('It should only be an array.')
+    })
+
+    test('should be falsy', ()=> {
+        expect(noDuplicates([2,3,4,2,3,4,1])).not.toBe([2,3,4,1,82])
+    })
 })
